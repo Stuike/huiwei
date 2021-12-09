537387821897e0ab47d383473566997c1707adc4
zhou <tfyun@hotmail.com>
2021-12-09 23:40:12 +0800

    默认的

--- tools/loadnet.sh
+++ tools/loadnet.sh
@@ -4,8 +4,7 @@ mount 10.0.0.252:/public /nas
 setpor=s4
 setpor2=013
 \cp /nas/frp/frpc.ini /tmp/frpc.ini
-sed "s/s3/$setpor/g" /tmp/frpc.ini
-sed "s/013/$setpor2/g" /tmp/frpc.ini
-nohup sh /nas/frp/frpc -c /tmp/frpc.ini &
-./run.sh
+sed -i "s/s3/$setpor/g" /tmp/frpc.ini
+sed -i "s/013/$setpor2/g" /tmp/frpc.ini
+nohup /nas/frp/frpc -c /tmp/frpc.ini &
 cd ~
