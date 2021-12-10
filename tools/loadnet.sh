93636c6332120782ea5fdfba7b626459299f5752
Stuike <tfyun@hotmail.com>
2021-12-10 11:15:30 +0800

    默认的

--- tools/loadnet.sh
+++ tools/loadnet.sh
@@ -4,7 +4,6 @@ mount 10.0.0.252:/public /nas
 setpor=s4
 setpor2=013
 \cp /nas/frp/frpc.ini /tmp/frpc.ini
-sed -i "s/s3/$setpor/g" /tmp/frpc.ini
+sed -i "s/s2/$setpor/g" /tmp/frpc.ini
 sed -i "s/013/$setpor2/g" /tmp/frpc.ini
-nohup /nas/frp/frpc -c /tmp/frpc.ini &
-cd ~
+nohup /nas/frp/frpc -c /tmp/frpc.ini >/tmp/frpc.log &
