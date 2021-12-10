08e42e77570709c948ab30b4d8d67afa5a4a52a9
zhou <tfyun@hotmail.com>
2021-12-10 22:19:00 +0800

    默认的

--- scripts/packageins/oneapi-install.sh
+++ scripts/packageins/oneapi-install.sh
@@ -2,6 +2,7 @@
 
 packages=/nas/HPCTools
 intel_ins_path=/opt/ohpc/pub/apps/intel
+mkdir -p /cachetmp/intel/
 
 BaseKit=2021.3.0.3422
 HPCKit=2021.4.0.3347
@@ -9,8 +10,8 @@ HPCKit=2021.4.0.3347
 #####install intel one api########
 echo "Install OneAPI BASEKit NOW!"
 ###extract and install#
-cp $packages/Intel/l_BaseKit_p_`echo $BaseKit`_offline.sh /cachetmp/Intel
-cd /cachetmp/Intel
+cp $packages/Intel/l_BaseKit_p_`echo $BaseKit`_offline.sh /cachetmp/intel/
+cd /cachetmp/intel
 sh l_BaseKit_p_`echo $BaseKit`_offline.sh -x
 cd l_BaseKit_p_`echo $BaseKit`_offline
 
@@ -19,8 +20,8 @@ cd l_BaseKit_p_`echo $BaseKit`_offline
 ./install.sh --components intel.oneapi.lin.dpcpp-cpp-compiler:intel.oneapi.lin.mkl.devel  --install-dir=$intel_ins_path --silent --eula accept
 
 echo "Install OneAPI HPCKit NOW!"
-cp $packages/Intel/l_HPCKit_p_`echo $HPCKit`_offline.sh /cachetmp/Intel
-cd /cachetmp/Intel
+cp $packages/Intel/l_HPCKit_p_`echo $HPCKit`_offline.sh /cachetmp/intel/
+cd /cachetmp/intel/
 sh l_HPCKit_p_`echo $HPCKit`_offline.sh -x
 cd l_HPCKit_p_`echo $HPCKit`_offline
 ##./install.sh --install-dir=/opt/ohpc/pub/apps/intel --silent --eula accept
--- tools/loadnet.sh
+++ tools/loadnet.sh
@@ -3,7 +3,7 @@ mount 10.0.0.252:/public /nas
 
 setpor=s4
 setpor2=013
-\cp /nas/frp/frpc.ini /tmp/frpc.ini
-sed -i "s/s2/$setpor/g" /tmp/frpc.ini
-sed -i "s/013/$setpor2/g" /tmp/frpc.ini
-nohup /nas/frp/frpc -c /tmp/frpc.ini >/tmp/frpc.log &
+\cp /nas/HPCTool/frp/frpc.ini /tmp/frpc.ini
+sed -i "s/NAmE/$setpor/g" /tmp/frpc.ini
+sed -i "s/POrT/$setpor2/g" /tmp/frpc.ini
+nohup /nas/HPCTool/frp/frpc -c /tmp/frpc.ini >/tmp/frpc.log &
